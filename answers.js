71423b87753ab67ad8d300422d52b75833d72c4a
Bryan LeBlanc <mrbeardbaron@gmail.com>
2017-01-13 00:22:33 +0000

    question 3-4

--- answers.js
+++ answers.js
@@ -18,4 +18,108 @@ console.log(getPositives);
 // and then printing the numbers from the filtered array.
 // Your code will look something like: return arr.filter(...).forEach(...)
 
-return nums.filter((x) => x>0).forEach((x) => console.log(x));
+//return nums.filter((x) => x>=0).forEach((x) => console.log(x));
+
+// Filter Array - Write a function called filterArray that takes a callback function and an array as arguments. 
+// Your filterArray function should return a new array that contains only the elements where the callback function
+// returns true. NOTE: This is a trick question. The answer is a one-liner and you are allowed to use array.filter
+var myArr = ["fourteen", 7, 55, -89, "Chicken"];
+var filterArray = Array.prototype.filter;
+
+
+// The Longest Word - Write a function called longestWord that takes a string as argument, 
+// and returns the longest word in the string. You should use Array.prototype.reduce to do your work.
+// Hint: You can use String.prototype.split to split the string into an array of words.
+var myString = "This is a bunch of words for this exercise!"
+var longestWord = function(str) {
+    return str.split(" ").reduce(function(longest, word) {
+        if (longest.length < word.length) {
+            longest = word;
+        }
+        return longest;
+    },"")
+    }
+    console.log(longestWord(myString));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// numArray2 = [5,4,9,33];
+// console.log(numArray2.reduce(function(a,b) {
+//     return a+b;
+// },0))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var theString = "We are trying to learn how 2 code ";
+// var theFunction = function(str) {
+//   return str.split(" ").filter(function(word) {
+//         if (word.match(/[a-z]/i)) {
+//             return true;
+//         }
+//         return false;
+//     })
+//     .reduce(function(shortest,word){
+//         if(shortest.length > word.length) {
+//             shortest = word;
+//         }
+//         return shortest;
+//     },"")
+// }
+// console.log(theFunction(theString));
