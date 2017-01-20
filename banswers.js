ba465dde5118b31bb7faa7b126aa8e2f6cdcbfc6
Bryan LeBlanc <mrbeardbaron@gmail.com>
2017-01-20 16:54:24 +0000

    ws3 redo

---
+++ banswers.js
@@ -0,0 +1,66 @@
+function printPositives(arr) {
+  arr.forEach(function(num) {
+    if (num > 0) {
+      console.log(num);
+    }
+  })
+}
+
+var numArray = [1,4,-4,34,-100,56,-423];
+
+printPositives(numArray);
+//
+function filterThenPrint(arr) {
+  arr.filter(function(num) {
+      return num > 0;
+  })
+    .forEach(function(num) {
+      console.log(num);
+    }
+  )
+}
+filterThenPrint(numArray);
+//
+var filterArray = Array.prototype.filter;
+//
+var myStr = "this is a really long string I wrote for the purposes of demonstrating longest word.";
+function longestWord(str) {
+  return str.split(" ").reduce(function(longest, word) {
+    if(longest.length < word.length) {
+      longest = word;
+    }
+     return longest;
+  }, "")
+}
+
+console.log(longestWord(myStr));
+//
+var qbf = "the quick brown fox";
+var vowelArr = ["a", "e", "i", "o", "u"]
+function countVowels(str) {
+  return str.toLowerCase().split("").reduce(function(count, letter) {
+    if(vowelArr.indexOf(letter) >= 0) {
+      count = count + 1;
+    }
+    return count;
+  }, 0);
+}
+console.log(countVowels(myStr));
+console.log(countVowels(qbf));
+//
+function highLow(arr) {
+  var highNum = arr.reduce(function(highest, num) {
+    if(highest < num) {
+      highest = num;
+    }
+    return highest;
+  }, 0);
+  var lowNum = arr.reduce(function(lowest, num) {
+    if(lowest > num) {
+      lowest = num;
+    }
+    return lowest;
+  }, 0);
+  return {highest: highNum, lowest: lowNum};
+}
+console.log(highLow(numArray));
